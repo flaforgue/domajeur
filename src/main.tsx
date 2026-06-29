c8402d3a16f30c7fe31a64fba878a979a7f138ba
Florian Laforgue <florian.laforgue@gmail.com>
2026-06-29 12:34:00 +0200

    feat: add pwa installation

--- src/main.tsx
+++ src/main.tsx
@@ -17,3 +17,9 @@ ReactDOM.createRoot(rootElement).render(
     </BrowserRouter>
   </React.StrictMode>,
 );
+
+if (import.meta.env.PROD && "serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js").catch(() => undefined);
+  });
+}
